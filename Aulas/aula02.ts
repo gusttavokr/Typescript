ce2b5c4e76d07d219214d9b6c526f2fdcfb0d0e9
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-03-21 18:41:16 +0000

    condicional

---
+++ Aulas/aula02.ts
@@ -0,0 +1,8 @@
+const age = 12
+
+if (age >= 18) {
+    alert ('Usuário maior de idade')
+}
+else {
+    alert("Usuário menor de idade")
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # typescript
+
+Quando usar const ou let?
+
+Constante é do início ao fim
+Let é para a declaração de variáveis mutáveis
