b8829c24e3003928fd6c1342abdc9824c40a1def
Gustavo Maia <gustavommilitaoo@gmail.com>
2025-03-21 18:32:16 +0000

    soma

--- Aulas/aula01.ts
+++ Aulas/aula01.ts
@@ -6,3 +6,9 @@ type User = {
 const fullName = (user: User): string => {
     return `${user.firstName} ${user.lastName}`
 }
+
+const sum = (value1: number, value2: number): number => {
+    return value1 + value2;
+}
+
+const total = sum(2, 2);
